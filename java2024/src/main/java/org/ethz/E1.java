67700d8adb233b5a026545bf1d713076b4a7f215
Tanja <tanja.falasca@uzh.ch>
2024-09-10 15:12:51 +0200

    day2

---
+++ java2024/src/main/java/org/ethz/E1.java
@@ -0,0 +1,15 @@
+package org.ethz;
+
+/**
+ * Exercise 1
+ *
+ */
+
+ public class E1 {
+     public static void main(String[] args) {
+         int x = 2;
+         for (; x < 10; x++) {
+             System.out.println(x++);
+         }
+     }
+ }
